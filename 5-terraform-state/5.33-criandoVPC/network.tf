6d30011d171bc3f39cb0858466e6a7bd76ca1395
Joao Victor <joaovictorxl120@gmail.com>
2024-09-06 16:10:06 -0300

    aula 33 | criacao de VPC com Remote State

---
+++ 5-terraform-state/5.33-criandoVPC/network.tf
@@ -0,0 +1,73 @@
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0/16"
+
+  tags = {
+    Name = "vpc-terraform-joao"
+  }
+}
+
+# Subrede
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet-terraform-joao"
+  }
+}
+
+# IGW
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "igw-terraform-joao"
+  }
+}
+
+# Route table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "route-table-terraform-joao"
+  }
+}
+
+# Associção de route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "security_group" {
+  name        = "security-group-terraform"
+  description = "Permitir acesso na porta 22"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # -1 permite todos os protocolos
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security-group-terraform"
+  }
+}
