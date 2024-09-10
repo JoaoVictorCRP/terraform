2689970e744286444f021570e4f79389a8202f64
Joao Victor <joaovictorxl120@gmail.com>
2024-09-10 15:21:47 -0300

    Aula 67 | terraform_data | Corrigindo AMI

--- 10-extras/10.67-data_nullResource/vm.tf
+++ 10-extras/10.67-data_nullResource/vm.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "key" {
 # VM
 resource "aws_instance" "vm" {
   ami                         = "ami-0e4d230445076ef91"
-  instance_type               = "t2.micro"
+  instance_type               = "t3.nano"
   key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
